95fc84f47366b83ea1c320cf8b199eab05f48712
Mridupawan Bordoloi <mridupawan503@gmail.com>
2026-05-29 00:38:51 +0530

    feat: initialize frontend project with full dashboard, transaction, and master data management modules

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
 import AuthContextProvider from "./contexts/AuthContext";
 import { ToastContainer } from "react-toastify";
